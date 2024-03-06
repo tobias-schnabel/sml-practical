fa51c504a9ebb163222a73a795926cab5cb02fd1
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-03-06 15:30:33 +0000

    created common setup script and added it to the top of each experimentation notebook

---
+++ Setup.py
@@ -0,0 +1,11 @@
+# Library Import
+import numpy as np
+import pandas as pd
+import sklearn as skl
+
+
+# Load the training data and the test inputs
+x_train = pd.read_csv('X_train.csv', index_col = 0, header=[0, 1, 2])
+y_train = pd.read_csv('y_train.csv', index_col=0)
+y_train = y_train.squeeze().to_numpy() # Make y_train a NumPy array
+x_test = pd.read_csv('X_test.csv', index_col = 0, header=[0, 1, 2])
