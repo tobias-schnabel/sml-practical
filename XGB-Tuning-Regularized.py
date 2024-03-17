ba1f71c3f0fdb0ef51e4e0e525a00837735a6128
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-03-17 15:55:16 +0000

    set up different tuning study for cpu

--- XGB-Tuning-Regularized.py
+++ XGB-Tuning-Regularized.py
@@ -39,32 +39,35 @@ X_real_test_scaled = scaler.transform(x_test)  # real test set we don't have lab
 
 
 def objective(trial):
-    # Hyperparameters to be tuned
+    # Suggest the number of boosting rounds
+    num_boost_round = trial.suggest_int('num_boost_round', 100, 1000)
+
     tuning_params = {
         'objective': 'multi:softmax',
         'num_class': 8,
-        'max_depth': trial.suggest_int('max_depth', 5, 100),
+        'booster': trial.suggest_categorical('booster', ['dart', 'gbtree']),
+        'max_depth': trial.suggest_int('max_depth', 1, 31),
         'eta': trial.suggest_float('eta', 0.005, 0.4),
         'subsample': trial.suggest_float('subsample', 0.6, 1.0),
         'colsample_bytree': trial.suggest_float('colsample_bytree', 0.6, 1.0),
-        'lambda': trial.suggest_float('lambda', 1e-8, 10.0, log=True),  # L2 regularization
-        'alpha': trial.suggest_float('alpha', 1e-8, 10.0, log=True),  # L1 regularization
-        'gamma': trial.suggest_float('gamma', 0.0, 5.0)  # Minimum loss reduction required for a split
+        'lambda': trial.suggest_float('lambda', 1e-8, 10.0, log=True),
+        'alpha': trial.suggest_float('alpha', 1e-8, 10.0, log=True),
+        'gamma': trial.suggest_float('gamma', 0.0, 5.0),
+        'n_jobs': -1,
+        # 'tree_method': 'gpu_hist',
+        'eval_metric': 'mlogloss'
     }
 
-    # Convert the dataset into DMatrix form
     dtrain = xgb.DMatrix(X_train_scaled, label=Y_train)
     dval = xgb.DMatrix(X_val_scaled, label=Y_val)
-
-    # List to hold the validation sets
     evals = [(dtrain, 'train'), (dval, 'validation')]
-    model = xgb.train(tuning_params, dtrain, num_boost_round=2_500, evals=evals,
-                      early_stopping_rounds=25, verbose_eval=False)
 
-    # Predictions on the validation set
+    # Pass num_boost_round to xgb.train
+    model = xgb.train(tuning_params, dtrain, num_boost_round=num_boost_round, evals=evals,
+                      early_stopping_rounds=30, verbose_eval=False)
+
     preds = model.predict(dval)
     accuracy = accuracy_score(Y_val, preds)
-
     return accuracy
 
 
@@ -79,8 +82,11 @@ params = {
     'num_class': 8,
 }
 
+
 # Update model parameters
 params.update(best_params)
+# Extract the best number of boosting rounds
+best_num_boost_round = best_params['num_boost_round']
 
 # Merge train and val set to retrain on maximal amount of data possible
 X_train_val_combined = np.vstack((X_train_scaled, X_val_scaled))
@@ -90,7 +96,7 @@ Y_train_val_combined = np.concatenate((Y_train, Y_val))
 dtrain_val_combined = xgb.DMatrix(X_train_val_combined, label=Y_train_val_combined)
 
 # Retrain the model on the full dataset with the best parameters
-final_model = xgb.train(params, dtrain_val_combined, num_boost_round=15_000)  # 5,000
+final_model = xgb.train(params, dtrain_val_combined, num_boost_round=best_num_boost_round)  # 5,000
 
 # Evaluate on the fake test set
 dtest = xgb.DMatrix(X_test_scaled)
@@ -104,6 +110,7 @@ formatted_test_accuracy = f"{test_accuracy * 100:.1f}"
 final_model.save_model(f'Models/xgboost-regularized-{formatted_test_accuracy}-all-data')
 
 # Run git commands to add the saved model file, commit, and push
+subprocess.run(['git', 'pull'], check=True)
 subprocess.run(['git', 'add', '.'], check=True)
 subprocess.run(['git', 'commit', '-m', 'tuning of xgb on all features completed'], check=True)
 subprocess.run(['git', 'push', 'origin', 'HEAD'], check=True)
